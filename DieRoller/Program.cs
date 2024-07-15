1ccb4df87d82df769ebe1bb1a70960402078449a
Scott <greatvstech@gmail.com>
2024-07-14 20:53:07 -0700

    Implement rolling functionality for Die class, and added test code to verify random FaceValue and Roll behavior

--- DieRoller/Program.cs
+++ DieRoller/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿   
+using DieRoller;
+
+Die myDie = new Die(); // When a die is created, a random FaceValue should be set
+// byte currentValue = myDie.FaceValue; // Getting face value should be allowed
+
+
+// Test code to make sure the die rolls and the face value changes
+Console.WriteLine(myDie.FaceValue);
+myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
+myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
+myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
+
+myDie.IsHeld = true;
+Console.WriteLine("Holding the die");
+myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
+myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
+
+Console.ReadKey();
